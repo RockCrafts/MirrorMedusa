57c28445855d5327fe9ac91a91a108020c4c2001
RockCrafts <44756861+RockCrafts@users.noreply.github.com>
2022-07-23 00:50:06 -0400

    PlayerMovement

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -6,6 +6,27 @@ public class PlayerMovement : MonoBehaviour
 {
    public float playerSpeed = 5f;
    
-   
+   public Rigidbody2D rb;
+   private Vector2 movement;
+
+   private void Start()
+   {
+      rb = GetComponent<Rigidbody2D>();
+   }
+  
+   private void Update() {
+      movement.x = Input.GetAxisRaw("Horizontal");
+      movement.y = Input.GetAxisRaw("Vertical");
+      // face direction of mouse
+      Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      transform.up = mousePosition - new Vector2(transform.position.x, transform.position.y);
+     
+
+   }
+   private void FixedUpdate()
+   {
+      rb.MovePosition(rb.position + movement * playerSpeed * Time.fixedDeltaTime);
+   }
+
 
 }
