c3075fbf48ed65f12e679e5a47ec41b336a87024
charliekeene <81133785+charliekeene@users.noreply.github.com>
2022-07-23 01:56:18 -0400

    Updated PlayerController to use forces
    the math is a bit weird but it makes movement feel a bit smoother and snappier, tweak linear drag and player speed as needed

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float playerSpeed = 5f;
-
+    public float playerSpeed = 10f;
+    private int speedPow = 2;
     public Rigidbody2D rb;
     private Vector2 movement;
 
@@ -17,15 +17,24 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement.Normalize();   // prevent moving faster diagonally
+        
         // face direction of mouse
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.up = mousePosition - new Vector2(transform.position.x, transform.position.y);
-
+    
 
     }
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * playerSpeed * Time.fixedDeltaTime);
+        //weird code, partially found on internet and partially my own design. was way too tired when i wrote this so maybe you can understand better than me
+        Vector2 targetSpeed = movement * playerSpeed;
+        Vector2 speedDiff = targetSpeed - rb.velocity;
+        float accelX = (Mathf.Abs(targetSpeed.x)>0.01f) ? 1 : -1;
+        float accelY = (Mathf.Abs(targetSpeed.y)>0.01f) ? 1 : -1;
+        Vector2 finalMove;
+        finalMove.x = Mathf.Pow(Mathf.Abs(speedDiff.x) * accelX, speedPow) * Mathf.Sign(speedDiff.x);
+        finalMove.y = Mathf.Pow(Mathf.Abs(speedDiff.y) * accelY, speedPow) * Mathf.Sign(speedDiff.y);
+        rb.AddForce(finalMove);
     }
 
 
